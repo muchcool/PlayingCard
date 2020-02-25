5cae820667ab759cc4db6b51cb7c2d7e1895da62
lano <lano@scs-hqmdeimac.local>
2020-02-25 15:58:17 +0800

    Initial Commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by lano on 2020/2/25.
+//  Copyright © 2020 SCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
